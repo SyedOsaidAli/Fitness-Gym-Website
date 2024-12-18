cbdca8bd0ab5f49e5b8393f7f9a61aed2ab70b0c
SyedOsaidAli <syedosaidali234@gmail.com>
2024-12-18 21:55:16 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
